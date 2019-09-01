490fc2cf29f7c82de82cc7b5cb49c2c979bd7793
Ivan Shcherbak <alotofall@gmail.com>
2019-09-01 17:29:09 +0300

    catch invalid parameters

--- src/App/SafeApp.php
+++ src/App/SafeApp.php
@@ -2,10 +2,14 @@
 
 namespace Funivan\CustomersRest\App;
 
+use Funivan\CustomersRest\App\Response\Error\ErrorBody;
 use Funivan\CustomersRest\App\Response\Error\ServerErrorResponse;
 use Funivan\CustomersRest\Http\Handler\Handler;
+use Funivan\CustomersRest\Http\Parameters\InvalidParameter;
 use Funivan\CustomersRest\Http\Request\ServerRequest;
+use Funivan\CustomersRest\Http\Response\PredefinedResponse;
 use Funivan\CustomersRest\Http\Response\Response;
+use Funivan\CustomersRest\Http\Response\Status\PredefinedStatus;
 
 /**
  * Decorator pattern
@@ -26,8 +30,15 @@ class SafeApp implements Handler
     {
         try {
             return $this->origin->handle($request);
-        } catch (\Exception $e) {
-            return new ServerErrorResponse($e);
+        } catch (InvalidParameter $exception) {
+            return new PredefinedResponse(
+                new PredefinedStatus(400, 'Bad Request'),
+                new ErrorBody(
+                    $exception->getMessage()
+                )
+            );
+        } catch (\Exception $exception) {
+            return new ServerErrorResponse($exception);
         }
     }
 }
