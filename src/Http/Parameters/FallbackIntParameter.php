cbfe318baa2327aba0135c472abeb04cd9edd9ea
Ivan Shcherbak <alotofall@gmail.com>
2019-09-01 12:46:54 +0300

    add fallback int

---
+++ src/Http/Parameters/FallbackIntParameter.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace Funivan\CustomersRest\Http\Parameters;
+
+use Funivan\CustomersRest\Spl\ArrayObject;
+use Funivan\CustomersRest\Spl\IntObject;
+
+class FallbackIntParameter implements IntObject
+{
+    /**
+     * @var ArrayObject
+     */
+    private $parameters;
+    /**
+     * @var string
+     */
+    private $key;
+    /**
+     * @var int
+     */
+    private $fallback;
+
+    public function __construct(ArrayObject $parameters, string $key, int $fallback)
+    {
+        $this->parameters = $parameters;
+        $this->key = $key;
+        $this->fallback = $fallback;
+    }
+
+    final public function toInt(): int
+    {
+        $data = $this->parameters[$this->key] ?? $this->fallback;
+        // Php is a strange language
+        if (is_int($data) || (is_numeric($data) && ((string)((int)$data)) === $data)) {
+            return (int)$data;
+        }
+        throw new InvalidParameter($this->key, 'invalid type, expect int');
+    }
+}
---
+++ src/Spl/IntObject.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Funivan\CustomersRest\Spl;
+
+
+interface IntObject
+{
+    public function toInt() : int;
+}
