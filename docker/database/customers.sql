8f2cdbfdc1423a0618826188460a9c0ab4ce4061
Ivan Shcherbak <alotofall@gmail.com>
2019-09-05 19:26:36 +0300

    use latest mysql version

--- docker/database/customers.sql
+++ docker/database/customers.sql
@@ -1,9 +1,7 @@
-create table customers
-(
-    "id" varchar(256) not null primary key,
-    "email" varchar(256) not null,
-    "name"  varchar(256) not null,
-    "last_name" varchar(256) null,
-    "update_time" bigint not null
+create table customers (
+    id varchar(256) not null primary key,
+    email varchar(256) not null,
+    name varchar(256) not null,
+    last_name varchar(256) null,
+    update_time bigint not null
 );
-
