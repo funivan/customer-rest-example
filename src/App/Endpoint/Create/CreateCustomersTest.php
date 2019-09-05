b0edd6b750f36bd6dd84aaa9d5e454569a67b429
Ivan Shcherbak <alotofall@gmail.com>
2019-09-05 14:33:02 +0300

    Add more test

--- src/App/Endpoint/Create/CreateCustomersTest.php
+++ src/App/Endpoint/Create/CreateCustomersTest.php
@@ -2,11 +2,12 @@
 
 use Funivan\CustomersRest\App\Endpoint\Create\CreateCustomers;
 use Funivan\CustomersRest\App\Repository\InMemory\InMemoryRepository;
+use Funivan\CustomersRest\Http\Parameters\InvalidParameter;
 use Funivan\CustomersRest\Http\Request\ServerRequest;
 use Funivan\CustomersRest\Spl\ArrayObject\PredefinedArray;
 
 return function () {
-    yield "Should create customer" => function () {
+    yield 'Should create customer' => function () {
         $data = [
             'customers' => [
                 [
@@ -25,5 +26,27 @@ return function () {
             new ServerRequest(null, new PredefinedArray($data), null));
         $ids = $response->body()->toArray()['ids'] ?? [];
         return count($ids) === 2;
+    };
+    yield 'Should show error on invalid field' => function () {
+        $data = [
+            'customers' => [
+                [
+                    'email' => 123,
+                ],
+            ]
+        ];
+        $e = null;
+        $message = '';
+        try {
+            /** @noinspection UnusedFunctionResultInspection */
+            (new CreateCustomers(new InMemoryRepository()))
+                ->handle(
+                    new ServerRequest(null, new PredefinedArray($data), null)
+                );
+        } catch (Exception $e) {
+            $message = $e->getMessage();
+        }
+        return $e instanceof InvalidParameter
+            && $message === 'Can not fetch parameter: email[invalid type, expect string]';
     };
 };
